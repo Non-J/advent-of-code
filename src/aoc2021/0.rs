8cdd30dc58224d20a69a84364c44c5e1221648a9
Non-j <works@jirawut.com>
2022-09-07 11:23:23 +0900

    Add template file

---
+++ src/aoc2021/0.rs
@@ -0,0 +1,25 @@
+const INPUT: &str = include_str!("data/day0.txt");
+
+pub fn entry() {
+    println!("{}", run_part1(INPUT));
+}
+
+fn run_part1(input: &str) -> u64 {
+    todo!()
+}
+
+fn run_part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[test]
+fn example_test_case_1() {
+    let input: &str = "";
+    assert_eq!(run_part1(input), 0)
+}
+
+#[test]
+fn example_test_case_2() {
+    let input: &str = "";
+    assert_eq!(run_part2(input), 0)
+}
